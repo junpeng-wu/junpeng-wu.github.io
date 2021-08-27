5d82a7623a73b59ca3a194614c7e051e04aadb5a
junpeng-wu <44920724+junpeng-wu@users.noreply.github.com>
2021-08-27 22:58:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||祝|宝|贝|生|日|快|乐|!|FOREVER|LOVE|#rectangle|');
+      S.UI.simulate('|#countdown 3||祝|宝|贝|生|日|快|乐|!|FOREVER|LOVE|YOU|#rectangle|');
     }
 
     S.Drawing.loop(function () {
